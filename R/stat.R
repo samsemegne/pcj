25546c65a9d88f7163f6b9c9cbdaef98432a3fc7
unknown <samsemegne@gmail.com>
2024-10-07 18:34:57 +0200

    Moves get_at() from to stat.R

--- R/stat.R
+++ R/stat.R
@@ -289,3 +289,227 @@ default_stats = function(x, ...) {
     probability = sample_proportion
   )
 }
+
+
+get_at = function(at, samples, stat) {
+  stopifnot(exprs = {
+    vek::is_num_vec_xyz(samples)
+    is.null(at) || vek::is_num_vec_xyz(at) || is.function(at) ||
+      vek::is_chr_vec_xb(at)
+  })
+
+  tmp = check_stat_result(stat, "stat")
+  if (!is_empty(tmp))
+    stop(tmp[[1L]])
+  else
+    rm(tmp)
+
+  new_pcj_safe_obj = \(k) {
+    return(structure(list(
+      condition = list(),
+      output = list(), # TODO is list type?
+      result = k
+    ), class = "pcj_result"))
+  }
+
+  if (is.null(at)) {
+    return(new_pcj_safe_obj(at))
+  }
+  else if (vek::is_num_vec_xyz(at)) {
+    return(new_pcj_safe_obj(at))
+  }
+  else if (is.function(at)) {
+    stopifnot(exprs = {
+      length(formals(at)) > 0L
+    })
+
+    at_obj = pcj_safely(at(samples))
+    return(at_obj)
+  } else if (vek::is_chr_vec_xb(at)) {
+    if (length(at) == 0L)
+      return(new_pcj_safe_obj(numeric(0L)))
+
+    supported_modes = c("mean", "median", "mean.default", "median.default")
+
+    is_q = startsWith(at, "q")
+    is_mode = at %in% supported_modes
+    q_str = at[is_q]
+    mode_str = at[is_mode]
+    lit_str = at[!(is_q | is_mode)]
+    rm(is_q, is_mode)
+
+    stopifnot(exprs = {
+      is_all_unique(at)
+      # TODO don't allow case both "q.5" and "median" %in% at
+    })
+
+    results = list()
+
+    lit = numeric(0L)
+    if (length(lit_str) > 0L) {
+      # Obtain literal values.
+      # TODO, do for each value individually, and don't throw error
+      lit = suppressWarnings(as.numeric(lit_str))
+      stopifnot(exprs = {
+        vek::is_num_vec_xyz(lit)
+        # TODO check for unique? else indexing at end may fail?
+      })
+
+      names(lit) = lit_str
+    }
+
+    q = numeric(0L)
+    if (length(q_str) > 0L) {
+      # Obtain quantiles.
+      q_val = sub(".", "", q_str)
+      q_val = suppressWarnings(as.numeric(q_val))
+      stopifnot(exprs = {
+        vek::is_num_vec_xyz(q_val)
+        all(q_val >= 0L, na.rm = FALSE)
+        all(q_val <= 1L, na.rm = FALSE)
+      })
+
+      q_obj = NULL
+      if (is_list(stat) && "quantile" %in% names(stat))
+      {
+        q_obj = pcj_safely(stat$quantile(samples, q_val))
+        q = q_obj$result
+
+      } else {
+        q_obj = pcj_safely(stats::quantile(samples, q_val))
+        q = q_obj$result
+      }
+
+      if (has_error(q_obj)) {
+        q = rep_len(NaN, length(q_str))
+        names(q) = q_str
+        results = c(results, list(quantile = q_obj))
+      } else {
+        e = NULL
+        if (!vek::is_num_vec(q))
+          e = e %||%
+            typeError('"quantile" must return a base-R numeric vector')
+
+        if (!vek::is_num_vec_xyz(q))
+          e = e %||% valueError('"quantile" result must be finite')
+
+        if (length(q) != length(q_val))
+          e = e %||%
+            valueError('"quantile" result length must equal input length')
+
+        r1 = rank(q, TRUE, "average")
+        r2 = rank(q_val, TRUE, "average")
+        names(r1) = NULL
+        names(r2) = NULL
+        if (!identical(r1, r2))
+          e = e %||%
+          valueError('"quantile" result rank must equal input rank')
+
+        rm(r1, r2)
+
+        if (any(q == q_val, na.rm = FALSE)) # TODO check
+          e = e %||% valueError('"quantile" results can\'t equal input')
+
+        if (!is.null(e))
+          q = rep_len(NaN, length(q_str))
+
+        tmp = new_pcj_safe_obj(NULL)
+        if (!is.null(e))
+          tmp$condition = list(e)
+
+        names(q) = q_str
+        results = c(results, list(quantile = q_obj, quantile_check = tmp))
+        rm(e)
+      }
+
+      #stopifnot(exprs = {
+      #  vek::is_num_vec_xyz(q)
+      #  length(q) == length(q_val)
+      #  identical(r1, r2)
+      #  all(q != q_val, na.rm = FALSE)
+      #})
+      #rm(r1, r2)
+
+
+      #names(q_obj$result) = q_str
+      #names(q) = q_str
+      #results = c(results, list(quantile = q_obj))
+    }
+
+    modes = numeric(0L)
+    if (length(mode_str) > 0L) {
+      # Obtain modes.
+      stopifnot(exprs = {
+        all(mode_str %in% supported_modes, na.rm = FALSE) # TODO "mode"
+      })
+
+      get_default_f = \(k) {
+        switch(
+          k,
+          "mean.default" = \(x) mean.default(x, trim = 0, na.rm = FALSE),
+          "median.default" = \(x) stats::median.default(x, na.rm = FALSE),
+          stop()
+        )
+      }
+
+      default_ops = c("mean.default", "median.default")
+      is_valid_mode = vek::is_num_vec_xyz1
+
+      obtain_mode_obj = \(m) {
+        if (m %in% default_ops) {
+          f = get_default_f(m)
+          m_obj = pcj_safely(f(samples))
+          return(m_obj)
+        }
+        else if (is_list(stat) && m %in% names(stat)) {
+          m_val = stat[[m]]
+          if (is.function(m_val)) {
+            f = m_val
+            m_obj = pcj_safely(f(samples))
+            return(m_obj)
+          } else if (is_valid_mode(m_val)) {
+            return(m_val)
+          } else {
+            stop()
+          }
+        } else {
+          stop()
+        }
+      }
+
+      mode_objects = lapply(mode_str, obtain_mode_obj)
+      names(mode_objects) = mode_str
+
+      get_mode = \(k) {
+        if (is.pcj_result(k)) {
+          if (is_valid_mode(k$result))
+            return(k$result)
+          else
+            return(NaN)
+        }
+        else if (is_valid_mode(k))
+          return(k)
+        else
+          stop()
+      }
+
+      modes = sapply(mode_objects, get_mode, simplify = TRUE, USE.NAMES = FALSE)
+      names(modes) = mode_str
+
+      results = c(results, Filter(is.pcj_result, mode_objects))
+    }
+
+    values = c(lit, q, modes)
+    # Put the values in the order of "at".
+    values = values[at]
+
+    return(structure(list(
+      condition = do.call(c, lapply(results, get_condition)),
+      output = list(), # TODO
+      result = values # Named numeric vector
+    ), class = "pcj_result"))
+  } else {
+    stop()
+  }
+}
+
